13c5d0183f62b90823855ac17a2831af5117079d
Canceltell <nekstalroma@gmail.com>
2025-10-17 23:37:05 +0300

    Add comp,length and etc

--- src/physics/math/vec2.c
+++ src/physics/math/vec2.c
@@ -1,4 +1,5 @@
 #include "vec2.h"
+#include <math.h>
 
 vec2_t
 vec_plus_vec(vec2_t vec1, vec2_t vec2) {
@@ -18,7 +19,26 @@ vec_mult_by_number(vec2_t vec, real n) {
                       .y = vec.y * n };
 }
 
+
 real
 get_scalar_product(vec2_t vec1, vec2_t vec2) {
     return vec1.x * vec2.x + vec1.y * vec2.y;
+}
+
+vec2_t
+length_vec(vec2_t vec){
+    return sqrt(vec.x * vec.x + vec.y * vec.y);    
+}
+
+vec2_t
+cross_vec(vec2_t vec1, vec2_t vec2){
+    return vec1.x * vec2.y - vec1.y * vec2.x;
+}
+
+vec2_t
+komp_vec(vec2_t vec1,vec2_t vec2){
+    return(vec2_t){
+    .x = vec1.x * vec2.x;
+    .y = vec1.y * vec2.y;
+    };
 }
--- src/physics/math/vec2.h
+++ src/physics/math/vec2.h
@@ -9,3 +9,6 @@ vec2_t vec_plus_vec(vec2_t vec1, vec2_t vec2);
 vec2_t vec_minus_vec(vec2_t vec1, vec2_t vec2);
 vec2_t vec_mult_by_number(vec2_t vec, real n);
 real get_scalar_product(vec2_t vec1, vec2_t vec2);
+vec2_t length_vec(vec2_t vec);
+vec2_t cross_vec(vec2_t vec1, vec2_t vec2);
+vec2_t komp_vec(vec2_t vec1, vec2_t vec2);
